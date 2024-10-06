65ec23362d5185bef564b6839d41b74bf5ee6c7d
zimop <zimopeng12345@gmail.com>
2024-10-06 11:06:21 +1100

    Added Car Model

---
+++ TodoApi/Controllers/CarsController.cs
@@ -0,0 +1,14 @@
+using CarStockApi.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CarStockApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase {
+        
+    }
+}
